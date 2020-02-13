d49d401bbf9a2ecc8bb5c8c2490da3510b70e891
ruthwang97 <46601888+ruthwang97@users.noreply.github.com>
2020-02-12 22:39:35 -0500

    a4

--- Assets/Collectible.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Collectible : MonoBehaviour
-{
-    public float value = 0.01f;
-    
-}
--- Assets/TreasureHunterInventory.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class TreasureHunterInventory : MonoBehaviour
-{
-    public List<Collectible> inventoryItems = new List<Collectible>();
-    // Start is called before the first frame update
-}

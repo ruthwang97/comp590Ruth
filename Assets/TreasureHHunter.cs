cee738044fb0f0259453078a5165c643aa21eb87
ruthwang97 <46601888+ruthwang97@users.noreply.github.com>
2020-02-12 23:36:53 -0500

    a4

---
+++ Assets/TreasureHHunter.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+#if UNITY_EDITOR
+    using UnityEditor;
+#endif
+
+
+public class TreasureHHunter : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("oo");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("oo");
+    }
+}
---
+++ Assets/TreasureHunterInventory.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreasureHunterInventory : MonoBehaviour
+{
+    public List<Collectible> inventoryItems = new List<Collectible>();
+    // Start is called before the first frame update
+}

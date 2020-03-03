8606b4eea21f457a2bb5cb2d18e27c7d8a60557f
ruthwang97 <46601888+ruthwang97@users.noreply.github.com>
2020-03-02 21:45:22 -0500

    A6

---
+++ Assets/trap.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class trap : MonoBehaviour
+{
+    public AudioSource audioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = this.gameObject.GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter(Collider other){
+        print("trigger");
+        audioSource.Play();
+        GameObject body = GameObject.Find("OVRPlayerController");
+        Rigidbody gameObjectRigidBody = body.AddComponent<Rigidbody>();
+        gameObjectRigidBody.mass = 5;
+    }
+}
